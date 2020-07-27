b913529adc5acf9556f77571d1f1326f8b76b9e1
seojeongyeon <wjddus9906@gmail.com>
2020-07-28 00:26:02 +0900

    오류수정

--- hackproject/urls.py
+++ hackproject/urls.py
@@ -35,12 +35,9 @@ urlpatterns = [
     path('mission_delete/<int:mission_id>', views.mission_delete, name='mission_delete'),
     path('mission_comment_create/<int:mission_id>', views.mission_comment_create, name='mission_comment_create'),
     path('mission_comment_delete/<int:comment_id>', views.mission_comment_delete, name='mission_comment_delete'),
-<<<<<<< HEAD
     path('commenting/<int:pk>', views.commenting, name= 'commenting'),
     path('comment_like/<int:pk>', views.comment_like, name='like'),
-=======
     path('mission_comment_like/<int:comment_id>', views.mission_comment_like, name='mission_comment_like'),
->>>>>>> a3322110ff1f557d2947f31df4e7b88fbe27bff3
     path('photowrite/', views.photowrite, name='photowrite'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
