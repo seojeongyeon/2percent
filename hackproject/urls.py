d80559b740eb349fc29a9745b0da9cadb0334278
seojeongyeon <wjddus9906@gmail.com>
2020-07-27 14:41:53 +0900

    add photoshop model

--- hackproject/urls.py
+++ hackproject/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from two import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +26,5 @@ urlpatterns = [
     path('photodetail/', views.photodetail, name='photodetail'),
     path('contest/', views.contest, name='contest'),
     path('mission/', views.mission, name='mission'),
-]
+    path('photowrite/', views.photowrite, name='photowrite'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- two/views.py
+++ two/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
-
+from .forms import PhotoshopForm
+from django.shortcuts import redirect
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def photoshop(request):
     return render(request, 'photoshop.html')
+
+def photowrite(request):
+    if request.method =='POST':
+        form = PhotoshopForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = PhotoshopForm()
+        return render(request, 'photowrite.html', {'form':form})
 
 def photodetail(request):
     return render(request, 'photodetail.html')
