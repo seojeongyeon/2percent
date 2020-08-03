b4357db042b648f11e8e69400c5d4cc2ccf7242e
dadahee <heeouo@kookmin.ac.kr>
2020-08-03 13:55:56 +0900

    error fix

--- account/urls.py
+++ account/urls.py
@@ -6,13 +6,12 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('signout/', views.signout, name='signout'),
 
-    #path('user/<int:user_id>', views.user, name="user"),
     path('user/<slug:username>', views.user, name="user"),
     path('mypage/', views.mypage, name="mypage"),
-    path('follower/<int:user_id>', views.follower, name="follower"),
-    path('following/<int:user_id>', views.following, name="following"),
-    path('follow/<int:user_id>', views.follow, name="follow"),
-    path('unfollow/<int:user_id>', views.unfollow, name="unfollow"),
+    path('follower/<slug:username>', views.follower, name="follower"),
+    path('following/<slug:username>', views.following, name="following"),
+    path('follow/<slug:username>', views.follow, name="follow"),
+    path('unfollow/<slug:username>', views.unfollow, name="unfollow"),
     path('info_setting/', views.info_setting, name="info_setting"),
     path('pw_setting/', views.pw_setting, name="pw_setting"),
 ]
